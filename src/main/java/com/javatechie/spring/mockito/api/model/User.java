591c893e8d3aa73e86f65e33aca332ebb887d80d
Krishna <krishnaprasad.vk@gmail.com>
2024-08-31 00:31:52 +0530

    Adding changes for mockito and jacoco

---
+++ src/main/java/com/javatechie/spring/mockito/api/model/User.java
@@ -0,0 +1,20 @@
+package com.javatechie.spring.mockito.api.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+	@Id
+	private int id;
+	private String name;
+	private int age;
+	private String address;
+
+	public User(int id, String name, int age, String address) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
+}
